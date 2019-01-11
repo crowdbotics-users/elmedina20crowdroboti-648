0a8ca03d088c439f796a70472d920953779728d7
Saurav <sp.gharti@gmail.com>
2019-01-11 18:26:45 +0000

    initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: waitress-serve --port=$PORT elmedina20crowdroboti_648.wsgi:application
---
+++ variables.tf
@@ -0,0 +1,12 @@
+
+variable "app_name" {
+  description = "Unique name of the app"
+  type = "string"
+  default = "elmedina20crowdroboti-648"
+}
+
+variable "custom_domain" {
+  description = "Custom domain name (optional)"
+  type = "string"
+  default = ""
+}
